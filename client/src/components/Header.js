d9c26bcd0b27a261d0a8435bffd7666a695a9d85
Steve Ferenchak <steveferenchak@gmail.com>
2019-05-13 21:24:09 -0700

    Create Basic Header and add Materialize-css

---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+	render() {
+		return (
+			<nav>
+				<div className='nav-wrapper'>
+					<a href='#' className='brand-logo'>
+						React-Emailer
+					</a>
+					<ul id='nav-mobile' className='right hide-on-med-and-down'>
+						<li>
+							<a href='sass.html'>Login with Google</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		);
+	}
+}
