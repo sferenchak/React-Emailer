2b1a088f6b031cc4f9bdbfdf8151c1ea44f968cf
Steve Ferenchak <steveferenchak@gmail.com>
2019-05-14 11:28:29 -0700

    Dynamic Header content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href='/auth/google'>Login with Google</a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a href='/api/logout'>Logout</a>
+					</li>
+				);
+		}
+	}
+
 	render() {
 		return (
 			<nav>
 				<div className='nav-wrapper'>
-					<a href='#' className='brand-logo'>
-						React-Emailer
-					</a>
-					<ul id='nav-mobile' className='right hide-on-small-and-down'>
-						<li>
-							<a href='sass.html'>Login with Google</a>
-						</li>
-					</ul>
+					<a className='left brand-logo'>React-Emailer</a>
+					<ul className='right'>{this.renderContent()}</ul>
 				</div>
 			</nav>
 		);
 	}
 }
+
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
