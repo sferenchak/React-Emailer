212dd4456fcfa7cbfc0f46c696aeaefb108e6837
Steve Ferenchak <steveferenchak@gmail.com>
2019-05-15 08:39:43 -0700

    Payments fixes

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -6,10 +6,14 @@ class Payments extends Component {
 		return (
 			<div>
 				<StripeCheckout
+					name='React-Emailer'
+					description='$5 for 5 email credits'
 					amount={500}
 					token={token => console.log(token)}
 					stripeKey={process.env.REACT_APP_STRIPE_KEY}
-				/>
+				>
+					<button className='btn'>Add Credits</button>
+				</StripeCheckout>
 			</div>
 		);
 	}
