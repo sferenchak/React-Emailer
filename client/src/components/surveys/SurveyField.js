f6084bc800b1dde8c5a74f22d2ef34ce2586f262
Steve Ferenchak <steveferenchak@gmail.com>
2019-05-28 11:11:06 -0700

    create SurveyForm/SurveyField; add to SurveyNew

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,13 @@
+// SurveyField contains logic to render a single
+// label and text input
+import React from 'react';
+
+const SurveyField = () => {
+	return (
+		<div>
+			<input />
+		</div>
+	);
+};
+
+export default SurveyField;
